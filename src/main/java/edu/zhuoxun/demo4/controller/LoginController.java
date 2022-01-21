ca8160cb662c3c75c68dd145d36a0916bcba77de
jash <hdmj@sina.com>
2022-01-21 16:40:12 +0800

    实现首页

--- src/main/java/edu/zhuoxun/demo4/controller/LoginController.java
+++ src/main/java/edu/zhuoxun/demo4/controller/LoginController.java
@@ -1,5 +1,8 @@
 package edu.zhuoxun.demo4.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import edu.zhuoxun.demo4.dao.ProductDao;
+import edu.zhuoxun.demo4.entry.Product;
 import edu.zhuoxun.demo4.entry.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 /**
  * Created by Jash
  */
@@ -17,10 +22,12 @@ import org.springframework.web.servlet.ModelAndView;
 public class LoginController {
     private final User admin;
 
-    public LoginController(User admin) {
-        this.admin = admin;
-    }
+    private ProductDao productDao;
 
+    public LoginController(User admin, ProductDao productDao) {
+        this.admin = admin;
+        this.productDao = productDao;
+    }
 
     @PostMapping("/login")
     @ResponseBody
@@ -36,6 +43,10 @@ public class LoginController {
     @GetMapping("/home")
     public ModelAndView home() {
         ModelAndView view = new ModelAndView("index");
+        List<Product> hots = productDao.selectList(new QueryWrapper<Product>().eq("is_hot", "1").orderByDesc("pdate").last("limit 9"));
+        List<Product> news = productDao.selectList(new QueryWrapper<Product>().orderByDesc("pdate").last("limit 9"));
+        view.addObject("hots", hots);
+        view.addObject("news", news);
         return view;
     }
 }
